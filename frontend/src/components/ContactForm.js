552751db1ad60d17bc748ae476c8d4a9f2343f2f
mrinmayray <mreenmayray@gmail.com>
2023-05-21 19:01:28 +0530

    Contact Form  Fixed

--- frontend/src/components/ContactForm.js
+++ frontend/src/components/ContactForm.js
@@ -4,12 +4,10 @@ function ContactForm() {
   return (
     <div className="form-container">
       <h1>Send a message to us!</h1>
-      <form>
-        <input placeholder="Name" />
-        <input placeholder="Email" />
-        <input placeholder="Subject" />
-        <textarea placeholder="Message" rows="4"></textarea>
-        <button>Send Message</button>
+      <form action="https://formsubmit.co/ec5dd8562a8477fe40a96ef6e254ed34" method="POST">
+        <input type="email" placeholder="Email" name="email" required />
+        <input type="text" placeholder="Message" name="name" required />
+        <button type="submit">Send</button>
       </form>
     </div>
   );
