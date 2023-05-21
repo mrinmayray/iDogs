d62f5cc1a51fb5a7379b90aa04f1b12feaa44da8
mrinmayray <mreenmayray@gmail.com>
2023-05-21 19:09:11 +0530

    Contact Fix

--- frontend/src/components/ContactForm.js
+++ frontend/src/components/ContactForm.js
@@ -5,8 +5,9 @@ function ContactForm() {
     <div className="form-container">
       <h1>Send a message to us!</h1>
       <form action="https://formsubmit.co/ec5dd8562a8477fe40a96ef6e254ed34" method="POST">
+        <input type="text" placeholder="Name" name="name" required />
         <input type="email" placeholder="Email" name="email" required />
-        <input type="text" placeholder="Message" name="name" required />
+        <textarea type="text" placeholder="Message" name="name" rows="4" required ></textarea>
         <button type="submit">Send</button>
       </form>
     </div>
