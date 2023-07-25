4e3492fbf65b96fd84d2af0c25f9ecff93df0f4a
Mrinmay Ray <mreenmayray@gmail.com>
2023-07-25 17:11:14 +0530

    str

--- frontend/src/utils/AllRoutes.js
+++ frontend/src/utils/AllRoutes.js
@@ -1,3 +1,3 @@
-export const host = "http://localhost:5000";
+export const host = "https://idogs.onrender.com";
 export const AdoptRegisterRoute = `${host}/api/auth/register`;
 export const AdoptListRoute = `${host}/api/auth/allusers`;
